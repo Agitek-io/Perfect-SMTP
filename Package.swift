70f41ddb0ba5dac1ec78954587f784975090743e
Stanislav Ageev <stanislav@agitek.io>
2023-02-09 14:19:46 +0300

    Fix compile error

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(name: "PerfectSMTP",
 	],
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
+        .package(url: "https://github.com/Agitek-io/Perfect-CURL.git", .branch("bug/curl_option_error")),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MIME.git", from: "1.0.0"),
 	],
